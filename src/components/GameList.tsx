51cf6801a40074d06e039a5bbc45c78dfa61ce73
NickEngle <nick.w.engle@gmail.com>
2025-11-05 11:30:43 -0500

    v2.2

--- src/components/GameList.tsx
+++ src/components/GameList.tsx
@@ -1,4 +1,4 @@
-﻿import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { Timestamp } from 'firebase/firestore';
 import {
   Badge,
